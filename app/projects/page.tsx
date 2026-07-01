5eab315860fba229463fbdb1b715570302707a8f
Leonardo Mac <leonaa2@uw.edu>
2026-07-01 14:09:08 -0700

    Added projects page to website

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,9 @@
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Projects</h1>
+      // TODO: create a flexbox
+      // TODO: invert dark mode of website
     </section>
   )
 }
