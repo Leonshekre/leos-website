5f75a0eaf989e8127f83e0a5a89cd993df12fce3
Leonardo Mac <leonaa2@uw.edu>
2026-07-15 15:55:05 -0700

    Added profile pic & simple introduction text to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,42 @@
 import { BlogPosts } from 'app/components/posts'
+import Image from 'next/image'
+import profilePic from 'app/public/images/leo_highschool_grad_photo.jpeg'
 
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Website In-Progress
       </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
-      </p>
-      <div className="my-8">
-        <BlogPosts />
+      <p className="text-4xl">Leonardo Paredes</p>
+      <Image 
+        src={profilePic}
+        alt="Picture of Leonardo Paredes"
+        className="my-8 w-32 h-32 rounded-full"
+      />
+      <div>
+        <p className="whitespace-pre-line">
+          Hello! <br></br>
+          I am a University of Washington alumni, recently graduated Dec 2025<br></br>
+          I am currently pursuing a career in IT Support while working my part-time job.<br></br>
+          <br></br>
+          <br></br>
+        </p>
+        <ul className="list-disc list-inside">
+          <p>Things I am currently studying/doing include...</p>
+          <li>Google IT Support Certificate (8.7% progress)</li>
+          <li>CompTia A+</li>
+          <p>And in a less structured way (for curiosity/fun)...</p>
+          <li>Xfinity Wifi mini Network Management Project</li>
+          <li>UTM Linux VM practice (on my Macbook)</li>
+          <li>Macbook & Linux exploration (Thinkpad T480 to buy & repair)</li>
+          <li>Portfolio website creation(this one!)</li>
+        </ul>
       </div>
+      
+      {/* <div className="my-8">
+        <BlogPosts />
+      </div> */}
     </section>
   )
 }
