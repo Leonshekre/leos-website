41b0140d2a3253b507e7a2ef5f05fee3fd445299
Leonardo875_Legion5i <leitoparal@gmail.com>
2026-07-30 11:50:56 -0700

    Dislike template. Starting from scratch

--- app/projects/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Projects</h1>
-      // TODO: create a flexbox
-      // TODO: invert dark mode of website
-    </section>
-  )
-}
